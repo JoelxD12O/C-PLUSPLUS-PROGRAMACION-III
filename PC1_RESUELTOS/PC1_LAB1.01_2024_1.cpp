80c243f6e91366f60a5d59558c691b9e60d48432
Joelxd <joel.cayllahua@utec.edu.pe>
2025-04-24 19:26:47 -0500

    mas actualiz

--- PC1_RESUELTOS/PC1_LAB1.01_2024_1.cpp
+++ PC1_RESUELTOS/PC1_LAB1.01_2024_1.cpp
@@ -2,31 +2,32 @@
 using namespace std;
 
 // Clase base generica para personajes
-template<typename T>
+template<typename T, typename U>
 class Personaje {
 private:
-    T nombre, ocupacion;
+    T nombre;
+    U ocupacion;
 public:
-    Personaje(T name, T ocupa) : nombre(name), ocupacion(ocupa) {}
+    Personaje(T name, U ocupa) : nombre(name), ocupacion(ocupa) {}
 
     virtual void usuario() = 0;
     virtual void anfitrion() = 0;
 
     T getNombre() { return nombre; }
-    T getOcupacion() { return ocupacion; }
+    U getOcupacion() { return ocupacion; }
 
     // Sobrecarga del operador << para mostrar la informacion del personaje
-    friend ostream& operator<<(ostream& out,  Personaje<T>& p) {
+    friend ostream& operator<<(ostream& out,  Personaje<T, U>& p) {
         out << "Nombre: " << p.getNombre() << ", Ocupacion: " << p.getOcupacion();
         return out;
     }
 };
 
 // Clase derivada para Usuario
-template<typename T>
-class Usuario : public Personaje<T> {
+template<typename T, typename U>
+class Usuario : public Personaje<T, U> {
 public:
-    Usuario(T name, T ocupa) : Personaje<T>(name, ocupa) {}
+    Usuario(T name, U ocupa) : Personaje<T, U>(name, ocupa) {}
 
     void usuario() override {
         cout << "Usuario: " << this->getNombre() << ", Ocupacion: " << this->getOcupacion() << endl;
@@ -38,10 +39,10 @@ public:
 };
 
 // Clase derivada para Anfitrion
-template<typename T>
-class Anfitrion : public Personaje<T> {
+template<typename T, typename U>
+class Anfitrion : public Personaje<T, U> {
 public:
-    Anfitrion(T name, T ocupa) : Personaje<T>(name, ocupa) {}
+    Anfitrion(T name, U ocupa) : Personaje<T, U>(name, ocupa) {}
 
     void usuario() override {
         cout << "Este es un anfitrion, no un usuario." << endl;
@@ -53,28 +54,29 @@ public:
 };
 
 // Clase base para Alojamientos
-template<typename T>
+template<typename T, typename U>
 class Alojamiento {
 protected:
-    T nombre, personas;
+    T nombre;
+    U personas;
 public:
-    Alojamiento(T name, T pers) : nombre(name), personas(pers) {}
+    Alojamiento(T name, U pers) : nombre(name), personas(pers) {}
 
     T getNombre() { return nombre; }
-    T getPersonas() { return personas; }
+    U getPersonas() { return personas; }
 
     // Sobrecarga del operador << para mostrar la informacion del alojamiento
-    friend ostream& operator<<(ostream& out,  Alojamiento<T>& a) {
+    friend ostream& operator<<(ostream& out,  Alojamiento<T, U>& a) {
         out << "Alojamiento: " << a.getNombre() << ", Personas: " << a.getPersonas();
         return out;
     }
 };
 
 // Clase derivada para Casa
-template<typename T>
-class Casa : public Alojamiento<T> {
+template<typename T, typename U>
+class Casa : public Alojamiento<T, U> {
 public:
-    Casa(T name, T pers) : Alojamiento<T>(name, pers) {}
+    Casa(T name, U pers) : Alojamiento<T, U>(name, pers) {}
 
     void mostrarInfo() {
         cout << "Casa: " << this->getNombre() << ", Personas: " << this->getPersonas() << endl;
@@ -82,10 +84,10 @@ public:
 };
 
 // Clase derivada para Habitaciones
-template<typename T>
-class Habitaciones : public Alojamiento<T> {
+template<typename T, typename U>
+class Habitaciones : public Alojamiento<T, U> {
 public:
-    Habitaciones(T name, T pers) : Alojamiento<T>(name, pers) {}
+    Habitaciones(T name, U pers) : Alojamiento<T, U>(name, pers) {}
 
     void mostrarInfo() {
         cout << "Habitacion: " << this->getNombre() << ", Personas: " << this->getPersonas() << endl;
@@ -93,16 +95,16 @@ public:
 };
 
 // Sobrecarga del operador + para "reservar" (simulando la suma de dias)
-template<typename T>
+template<typename T, typename U>
 class Reserva {
 private:
     T nombre;
-    int dias;
+    U dias;
 public:
-    Reserva(T name, int d) : nombre(name), dias(d) {}
+    Reserva(T name, U d) : nombre(name), dias(d) {}
 
     // Sobrecarga del operador + para sumar dias a la reserva
-    Reserva operator+(int extraDias) {
+    Reserva operator+(U extraDias) {
         return Reserva(this->nombre, this->dias + extraDias);
     }
 
@@ -110,13 +112,14 @@ public:
         cout << "Alojamiento: " << nombre << " | Dias reservados: " << dias << endl;
     }
 };
+
 // Función para imprimir información de múltiples personajes o alojamientos
-template<typename T, typename... Args>
-void imprimirInformacion(T& t, Args&... args) {
+template<typename T, typename U, typename... Args>
+void imprimirInformacion(T& t, U& u, Args&... args) {
     cout << t << endl;
+    cout << u << endl;
     ((cout << args << endl),...);  // Expansión de los argumentos
 }
-
 
 // Función para mostrar el menú y ejecutar las opciones seleccionadas
 void mostrarMenu() {
@@ -128,12 +131,12 @@ void mostrarMenu() {
 
 int main() {
 
-    Usuario<string> usuario1("Juan", "Estudiante");
-    Anfitrion<string> anfitrion1("Carlos", "Propietario");
-    Casa<string> casa1("Casa en la playa", "5");
-    Habitaciones<string> habitacion1("Habitacion privada", "2");
+    Usuario<string, string> usuario1("Juan", "Estudiante");
+    Anfitrion<string, string> anfitrion1("Carlos", "Propietario");
+    Casa<string, int> casa1("Casa en la playa", 5);
+    Habitaciones<string, int> habitacion1("Habitacion privada", 2);
 
-    Reserva<string> reserva1("Casa en la playa", 7);
+    Reserva<string, int> reserva1("Casa en la playa", 7);
 
     int opcion;
     do {
@@ -146,27 +149,27 @@ int main() {
         switch (opcion) {
             case 1:
                 // Imprimir la información de todos los objetos
-                    imprimirInformacion(usuario1, anfitrion1, casa1, habitacion1);
-            break;
+                imprimirInformacion(usuario1, anfitrion1, casa1, habitacion1);
+                break;
 
             case 2:
                 // Agregar días a la reserva
-                    reserva1 = reserva1 + 3;  // Por ejemplo, sumar 3 días
-            reserva1.mostrarReserva();  // Mostrar la reserva con los nuevos días
-            break;
+                reserva1 = reserva1 + 3;  // Por ejemplo, sumar 3 días
+                reserva1.mostrarReserva();  // Mostrar la reserva con los nuevos días
+                break;
 
             case 3:
                 cout << "Saliendo del programa..." << endl;
-            break;
+                break;
 
             default:
                 cout << "Opcion no valida, intente nuevamente." << endl;
-            break;
+                break;
         }
     } while (opcion != 3);  // Salir cuando se elija la opción 3
 
     return 0;
-}
+};
     /*
      //TEST2
     // Crear objetos
